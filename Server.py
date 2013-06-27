d18511958b5d4dfe0c4efd2dda7ae482b40b2372
Cesar <cesar.chavez@petrologautomation.com>
2013-06-26 19:46:15 -0500

    Test Github commits

--- Server.py
+++ Server.py
@@ -15,8 +15,10 @@ import serial
 port = serial.Serial("/dev/ttyAMA0", baudrate=19200, timeout=1.0)
 while True:
     port.write("00S?1\r\n")
-    print (port.readline())
+    print(port.readline())
     port.write("00MB\r\n")
-    print (port.readline())
+    print(port.readline())
     port.write("00E\r\n")
-    print (port.readline())
+    print(port.readline())
+
+    print('test GitHub')
